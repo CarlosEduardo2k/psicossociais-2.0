f4692be51cd0d53f5ab8ef2c988701ac0aa92dc5
Carlos Eduardo <Carloseduardo141724@gmail.com>
2026-09-03 16:12:56 -0300

    Inicializa projeto Spring Boot

---
+++ Back-end/src/main/java/br/com/psicossocial/AvaliacaoPsicossocialApplication.java
@@ -0,0 +1,13 @@
+package br.com.psicossocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoPsicossocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoPsicossocialApplication.class, args);
+	}
+
+}
